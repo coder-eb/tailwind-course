a5e1b035376a3698f1b4aa1fbe199c6baae0c7af
lazy_eb <ebranbright@gmail.com>
2022-12-11 23:09:15 +0530

    Add Hamburger Icon

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,33 +1,30 @@
-function SubMenuItem({itemName}) {
-    return (
-        <div className="p-4 font-bold hover:bg-white/5 hover:text-zinc-200 transition-colors ease-in-out cursor-pointer text-pink-200">
-            <span>{itemName}</span>
-        </div>
-    )
+function SubMenuItem({ itemName }) {
+  return (
+    <div className="cursor-pointer p-4 font-bold text-pink-200 transition-colors ease-in-out hover:bg-white/5 hover:text-zinc-200">
+      <span>{itemName}</span>
+    </div>
+  );
 }
 
-function SubMenu({items}) {
-    if (items.length > 0) {
-        const subMenu = items.map((item) =>
-            <SubMenuItem itemName={item}/>
-        );
-        return (
-            <div className="group-hover:block hidden absolute top-full right-0 whitespace-nowrap bg-pink-500 rounded-b-md text-right">
-                {subMenu}
-            </div>
-        )
-    }
-    return (<></>)
-}
-
-function MenuItem({name, subMenuItems}) {
-    
+function SubMenu({ items }) {
+  if (items.length > 0) {
+    const subMenu = items.map((item) => <SubMenuItem itemName={item} />);
     return (
-        <div className="group relative flex h-full items-center p-4 cursor-pointer font-bold text-pink-200 hover:text-zinc-200 hover:bg-white/10 transition-colors ease-in-out">
-            <span>{name}</span>
-            <SubMenu items={subMenuItems}/>
-        </div>
+      <div className="absolute top-full right-0 hidden whitespace-nowrap rounded-b-md bg-pink-500 text-right group-hover:block">
+        {subMenu}
+      </div>
     );
+  }
+  return <></>;
+}
+
+function MenuItem({ name, subMenuItems = [] }) {
+  return (
+    <div className="group relative flex h-full cursor-pointer items-center p-4 font-bold text-pink-200 transition-colors ease-in-out hover:bg-white/10 hover:text-zinc-200">
+      <span>{name}</span>
+      <SubMenu items={subMenuItems} />
+    </div>
+  );
 }
 
 export default MenuItem;
