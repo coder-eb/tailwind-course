fccb6092147333a3ecee43312cf04c874336930b
lazy_eb <ebranbright@gmail.com>
2022-12-17 23:22:07 +0530

    Create Hero Section

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
+import HeroSection from "./HeroSection";
 import NavBar from "./navigation/NavBar";
 
 function Header() {
     return (
       <header className="text-zinc-200">
         <NavBar/>
+        <HeroSection/>
       </header>
     );
 }
